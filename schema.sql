6cad665aa007a8f552586ec8162eb104c39762cb
James Cook <63876549+jamescook98@users.noreply.github.com>
2020-08-15 12:58:16 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -21,5 +21,7 @@ CREATE TABLE userPost (
   postTitle VARCHAR(140) NOT NULL,
   postBody NVARCHAR(8000) NOT NULL,
   postTags NVARCHAR(8000) NULL,
+  userRating int NOT NULL,
+  imgFilepath VARCHAR(160) NULL,
   PRIMARY KEY (id)
 );
